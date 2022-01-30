1b420aff394a27cbb381d356ef427cf0bfc63cf0
Nickita V <hihollow@yandex.ru>
2022-01-30 23:11:37 +0600

    homework1
    homework1

---
+++ ProjectAlgorithm/Fibonacchi/Lesson1_3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fibonacchi
+{
+    class Lesson1_3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число(колличество чисел в ряду Фибоначчи)");
+            long n = long.Parse(Console.ReadLine());
+            Fib fib = new Fib();
+            fib.FibRekurs(n);
+            fib.FibСycle(n);
+
+
+        }
+    }
+}
---
+++ ProjectAlgorithm/ProjectAlgorithm/Lesson1_1 .cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjectAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            long number = long.Parse(Console.ReadLine());
+            primeNumberTest prime = new primeNumberTest();
+            prime.NumberTest(number);
+
+
+
+           
+
+        }
+    }
+}
